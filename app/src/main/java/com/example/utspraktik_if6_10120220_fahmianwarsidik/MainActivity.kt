4cf64be40c56372cefb21a4617c1ce8aee4fbe22
unknown <fahmisidik794@gmail.com>
2023-05-26 17:52:54 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10120220_fahmianwarsidik/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.utspraktik_if6_10120220_fahmianwarsidik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//Tanggal Pengerjaan : 26-05-2023
+//NIM                : 10120220
+//Nama               : Fahmi Anwar Sidik
+//Kelas              : IF6
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
